7124e2676e358e145af279c4e1c10807e51fd8dc
AMicciche <alessiomicciche@outlook.com>
2026-03-25 13:02:19 +0100

    Add Category and Project resources with CRUD functionality; implement forms and tables for managing categories and projects; add service resource with corresponding pages and schemas.

---
+++ app/Filament/Resources/Projects/Schemas/ProjectForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\Projects\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class ProjectForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('category_id')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('title')
+                    ->required(),
+                TextInput::make('slug')
+                    ->required(),
+                TextInput::make('location'),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Services/Schemas/ServiceForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Services\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class ServiceForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                TextInput::make('slug')
+                    ->required(),
+                Textarea::make('content')
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('icon'),
+            ]);
+    }
+}
